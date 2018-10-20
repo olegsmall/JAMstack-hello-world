762194165dac51c8b1d0b4f8832ccd53392b3741
Oleg Smolovyk <smolovyk.oleg@gmail.com>
2018-10-20 01:51:31 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>I'm Oleg Smolovykddd</p>
+    <p>I'm Oleg Smolovy2</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
